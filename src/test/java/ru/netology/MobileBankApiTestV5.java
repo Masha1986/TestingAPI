43acead5db28f7bf39d89ee4196d65d0f2160082
Masha1986 <9312613800@mail.ru>
2022-02-23 20:27:14 +0300

    Change 0 on -1

--- src/test/java/ru/netology/MobileBankApiTestV5.java
+++ src/test/java/ru/netology/MobileBankApiTestV5.java
@@ -17,12 +17,12 @@ class MobileBankApiTestV5 {
                 .baseUri("http://localhost:9999/api/v1")
                 // Выполняемые действия
                 .when()
-                .get("/demo/accounts")git
-           A     // Проверки
+                .get("/demo/accounts")
+                // Проверки
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("every{ it.balance >= 0" }", is(true))
+                .body("every{ it.balance >= -1 }", is(true))
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
-}""
+}
